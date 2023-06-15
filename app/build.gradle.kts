d2f82eaa10378890b8e8c9f2aadb1b9e90f715ea
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2023-06-15 13:54:24 -0400

    ktlint setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,9 @@
-import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
 
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 
 android {
     namespace = "com.example.moviefy_clean_architecture"
@@ -33,10 +32,10 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.core:core-ktx:1.8.0")
-    implementation ("androidx.appcompat:appcompat:1.4.1")
-    implementation ("com.google.android.material:material:1.5.0")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+apply(from = "gradle/ktlint.gradle.kts")
+
 plugins {
-    id ("com.android.application") version "8.0.2" apply false
-    id ("com.android.library") version "8.0.2" apply false
-    id ("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
