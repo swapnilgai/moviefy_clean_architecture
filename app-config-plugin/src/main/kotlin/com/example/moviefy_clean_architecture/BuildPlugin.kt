dcc9a4be69dbb8016e6ffc7979419f469ebc986a
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2023-06-16 14:33:36 -0400

    api key integration

--- app-config-plugin/src/main/kotlin/com/example/moviefy_clean_architecture/BuildPlugin.kt
+++ app-config-plugin/src/main/kotlin/com/example/moviefy_clean_architecture/BuildPlugin.kt
@@ -1,9 +1,36 @@
 package com.example.moviefy_clean_architecture
 
+import ProjectProperties
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
+import java.util.Properties
 
 class BuildPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+
+
     }
+}
+
+
+private fun Project.loadProperties(): ProjectProperties {
+
+    val projectProperties = Properties()
+
+    projectProperties.load(
+        InputStreamReader(
+            ByteArrayInputStream(
+                providers.fileContents(layout.projectDirectory.file("../apikey.properties"))
+                    .asBytes
+                    .forUseAtConfigurationTime()
+                    .orNull
+                    ?: throw GradleException("Mission apikey.properties")
+            )
+        )
+    )
+
+    return ProjectProperties(apiKey = projectProperties.getProperty("API_KEY"))
 }
